3d3ef13d832ce20961a05ab62bd6d5e98af5e376
therusetiawan <me.herusetiawan@gmail.com>
2025-03-23 18:38:37 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,112 @@
+# README.md
+
+## Student Management API
+This is a simple Student Management API built with Golang, Gin, GORM, AWS S3, and Redis.
+
+### Features
+- Create, Read, Update, and Delete (CRUD) students
+- Upload student photos to AWS S3
+- Store student data in MySQL and cache in Redis
+
+### Prerequisites
+- Docker
+- Docker Compose
+- AWS S3 Credentials
+
+### Environment Variables
+Create a `.env` file and configure the following environment variables:
+```env
+DB_USER=root
+DB_PASS=password
+DB_HOST=db
+DB_PORT=3306
+DB_NAME=student_db
+
+REDIS_ADDR=redis:6379
+
+AWS_REGION=us-east-1
+AWS_ACCESS_KEY=your_access_key
+AWS_SECRET_KEY=your_secret_key
+AWS_BUCKET_NAME=your_bucket_name
+```
+
+### How to Run with Docker
+1. Build and run the container:
+   ```sh
+   docker-compose up --build -d
+   ```
+2. The API will be available at `http://YOUR_IP_ADDRESS:8080`
+
+### API Endpoints
+| Method | Endpoint               | Description              |
+|--------|------------------------|--------------------------|
+| POST   | `/students`            | Create a student        |
+| GET    | `/students`            | Get all students        |
+| GET    | `/students/:id`        | Get student by ID       |
+| PUT    | `/students/:id`        | Update student          |
+| DELETE | `/students/:id`        | Delete student          |
+| GET    | `/students/cache/:id`  | Get student from cache  |
+
+---
+
+# Dockerfile
+
+```dockerfile
+FROM golang:1.19-alpine
+
+WORKDIR /app
+
+COPY go.mod go.sum ./
+RUN go mod download
+
+COPY . .
+
+RUN go build -o main .
+
+EXPOSE 8080
+
+CMD ["./main"]
+```
+
+---
+
+# docker-compose.yml
+
+```yaml
+version: '3.8'
+
+services:
+  app:
+    build: .
+    ports:
+      - "8080:8080"
+    depends_on:
+      - db
+      - redis
+    environment:
+      - DB_USER=root
+      - DB_PASS=password
+      - DB_HOST=db
+      - DB_PORT=3306
+      - DB_NAME=student_db
+      - REDIS_ADDR=redis:6379
+      - AWS_REGION=us-east-1
+      - AWS_ACCESS_KEY=your_access_key
+      - AWS_SECRET_KEY=your_secret_key
+      - AWS_BUCKET_NAME=your_bucket_name
+    restart: always
+
+  db:
+    image: mariadb:latest
+    environment:
+      - MYSQL_ROOT_PASSWORD=password
+      - MYSQL_DATABASE=student_db
+    ports:
+      - "3306:3306"
+    restart: always
+
+  redis:
+    image: redis:latest
+    ports:
+      - "6379:6379"
+    restart: always
---
+++ main.go
@@ -0,0 +1,225 @@
+package main
+
+import (
+  "context"
+  "encoding/json"
+  "fmt"
+  "io"
+  "log"
+  "net/http"
+  "os"
+  "strconv"
+
+  "github.com/aws/aws-sdk-go/aws"
+  "github.com/aws/aws-sdk-go/aws/credentials"
+  "github.com/aws/aws-sdk-go/aws/session"
+  "github.com/aws/aws-sdk-go/service/s3"
+  "github.com/gin-gonic/gin"
+  "github.com/go-redis/redis/v8"
+  "gorm.io/driver/mysql"
+  "gorm.io/gorm"
+)
+
+// Student Model
+type Student struct {
+  ID     uint   `json:"id" gorm:"primaryKey"`
+  Name   string `json:"name"`
+  School string `json:"school"`
+  Photo  string `json:"photo"`
+}
+
+// Global Variables
+var (
+  db       *gorm.DB
+  rdb      *redis.Client
+  ctx      = context.Background()
+  s3Client *s3.S3
+)
+
+// Initialize MariaDB Connection
+func initDB() {
+  dbUser := os.Getenv("DB_USER")
+  dbPass := os.Getenv("DB_PASS")
+  dbHost := os.Getenv("DB_HOST")
+  dbPort := os.Getenv("DB_PORT")
+  dbName := os.Getenv("DB_NAME")
+
+  dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+    dbUser, dbPass, dbHost, dbPort, dbName)
+
+  var err error
+  db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+  if err != nil {
+    log.Fatal("Failed to connect to database:", err)
+  }
+
+  db.AutoMigrate(&Student{})
+}
+
+// Initialize Redis Connection
+func initRedis() {
+  rdb = redis.NewClient(&redis.Options{
+    Addr:     os.Getenv("REDIS_ADDR"),
+    Password: "",
+    DB:       0,
+  })
+
+  _, err := rdb.Ping(ctx).Result()
+  if err != nil {
+    log.Fatal("Failed to connect to Redis:", err)
+  }
+}
+
+// Initialize AWS S3
+func initS3() {
+  sess, err := session.NewSession(&aws.Config{
+    Region: aws.String(os.Getenv("AWS_REGION")),
+    Credentials: credentials.NewStaticCredentials(
+      os.Getenv("AWS_ACCESS_KEY"),
+      os.Getenv("AWS_SECRET_KEY"),
+      "",
+    ),
+  })
+  if err != nil {
+    log.Fatal("Failed to initialize AWS S3 session:", err)
+  }
+
+  s3Client = s3.New(sess)
+}
+
+// Upload Photo to AWS S3
+func uploadPhoto(file io.Reader, fileName string) (string, error) {
+  bucket := os.Getenv("AWS_BUCKET_NAME")
+
+  _, err := s3Client.PutObject(&s3.PutObjectInput{
+    Bucket: aws.String(bucket),
+    Key:    aws.String(fileName),
+    Body:   file,
+    ACL:    aws.String("public-read"),
+  })
+  if err != nil {
+    return "", err
+  }
+
+  return fmt.Sprintf("https://%s.s3.amazonaws.com/%s", bucket, fileName), nil
+}
+
+// Create Student (with Photo Upload)
+func createStudent(c *gin.Context) {
+  name := c.PostForm("name")
+  school := c.PostForm("school")
+  file, header, err := c.Request.FormFile("photo")
+
+  if err != nil {
+    c.JSON(http.StatusBadRequest, gin.H{"error": "Photo is required"})
+    return
+  }
+  defer file.Close()
+
+  photoURL, err := uploadPhoto(file, header.Filename)
+  if err != nil {
+    c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to upload photo"})
+    return
+  }
+
+  student := Student{Name: name, School: school, Photo: photoURL}
+  db.Create(&student)
+
+  // Store in Redis cache
+  studentJSON, _ := json.Marshal(student)
+  rdb.Set(ctx, fmt.Sprintf("student:%d", student.ID), studentJSON, 0)
+
+  c.JSON(http.StatusOK, student)
+}
+
+// Get All Students
+func getStudents(c *gin.Context) {
+  var students []Student
+  db.Find(&students)
+  c.JSON(http.StatusOK, students)
+}
+
+// Get Student By ID
+func getStudentByID(c *gin.Context) {
+  id := c.Param("id")
+  var student Student
+  result := db.First(&student, id)
+
+  if result.Error != nil {
+    c.JSON(http.StatusNotFound, gin.H{"error": "Student not found"})
+    return
+  }
+
+  c.JSON(http.StatusOK, student)
+}
+
+// Update Student
+func updateStudent(c *gin.Context) {
+  id := c.Param("id")
+  var student Student
+  result := db.First(&student, id)
+
+  if result.Error != nil {
+    c.JSON(http.StatusNotFound, gin.H{"error": "Student not found"})
+    return
+  }
+
+  name := c.PostForm("name")
+  school := c.PostForm("school")
+
+  if name != "" {
+    student.Name = name
+  }
+  if school != "" {
+    student.School = school
+  }
+
+  db.Save(&student)
+
+  // Update cache
+  studentJSON, _ := json.Marshal(student)
+  rdb.Set(ctx, fmt.Sprintf("student:%d", student.ID), studentJSON, 0)
+
+  c.JSON(http.StatusOK, student)
+}
+
+// Delete Student
+func deleteStudent(c *gin.Context) {
+  id := c.Param("id")
+  db.Delete(&Student{}, id)
+
+  // Remove from Redis cache
+  rdb.Del(ctx, fmt.Sprintf("student:%s", id))
+
+  c.JSON(http.StatusOK, gin.H{"message": "Student deleted"})
+}
+
+// Get Student from Cache
+func getStudentCache(c *gin.Context) {
+  id := c.Param("id")
+  data, err := rdb.Get(ctx, fmt.Sprintf("student:%s", id)).Result()
+
+  if err != nil {
+    c.JSON(http.StatusNotFound, gin.H{"error": "Student not found in cache"})
+    return
+  }
+
+  c.String(http.StatusOK, data)
+}
+
+// Main function
+func main() {
+  initDB()
+  initRedis()
+  initS3()
+
+  r := gin.Default()
+  r.POST("/students", createStudent)
+  r.GET("/students", getStudents)
+  r.GET("/students/:id", getStudentByID)
+  r.PUT("/students/:id", updateStudent)
+  r.DELETE("/students/:id", deleteStudent)
+  r.GET("/students/cache/:id", getStudentCache)
+
+  r.Run(":8080")
+}
